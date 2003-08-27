729841158d3eae8be4843b0e929f07f2fc5a82cf
Simon Urbanek <simon.urbanek@r-project.org>
2003-08-27 17:31:48 +0000

    Added relevant prototypes from callJNI.c
    git-svn-id: svn://svn.rforge.net/rJava/trunk@10 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/rJava.h
+++ src/rJava.h
@@ -7,4 +7,18 @@
 extern JNIEnv *env;
 extern JavaVM *jvm;
 
+/* in callJNI */
+jobject createObject(char *class, char *sig, jvalue *par);
+jclass getClass(char *class);
+
+jdoubleArray newDoubleArray(double *cont, int len);
+jintArray newIntArray(int *cont, int len);
+jbooleanArray newBooleanArrayI(int *cont, int len);
+jstring newString(char *cont);
+
+void releaseObject(jobject o);
+jobject makeGlobal(jobject o);
+void releaseGlobal(jobject o);
+
+void printObject(jobject o);
 #endif
