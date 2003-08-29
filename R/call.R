719b245ad8736323753e4e2b5ea41da97bb3056c
Simon Urbanek <simon.urbanek@r-project.org>
2003-08-29 14:08:21 +0000

    handle arrays returned in .jcall
    git-svn-id: svn://svn.rforge.net/rJava/trunk@19 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- R/call.R
+++ R/call.R
@@ -12,14 +12,30 @@
   o
 }
 
-.jcall <- function(obj, returnSig="V", method, ...) {
+.jcall <- function(obj, returnSig="V", method, ..., evalArray=FALSE) {
   r<-NULL
   if (inherits(obj,"jobjRef")) {
+    if (returnSig=="S")
+      returnSig<-"Ljava/lang/String;"
+    if (returnSig=="[S")
+      returnSig<-"[Ljava/lang/String;"
     r<-.External("RcallMethod",obj$jobj,returnSig, method, ..., PACKAGE="rJava")
-    if (substr(returnSig,1,1)=="L") {
+    if (substr(returnSig,1,1)=="[") {
+      if (evalArray) {
+        if (returnSig=="[I")
+          return(.External("RgetIntArrayCont", r))
+        else if (returnSig=="[D")
+          return(.External("RgetDoubleArrayCont", r))
+        else if (substr(returnSig,1,2)=="[L") # this one is wrong - we'll get all the jobjects, but we'd have to convert them to R objects first
+          return(.External("RgetObjectArrayCont", r))
+      }
+      r<-list(jobj=r, jclass=NULL, jsig=returnSig)
+      class(r)<-c("jarrayRef","jobjRef")
+    } else if (substr(returnSig,1,1)=="L") {
       r<-list(jobj=r, jclass=substr(returnSig,2,nchar(returnSig)-1))
       class(r)<-"jobjRef"
     }
+    
     .C("checkExceptions")
   } else { # call for static methods
   }
@@ -34,6 +50,9 @@
 }
 
 .jstrVal <- function(obj) {
+  # .jstrVal(.jstrVal(...)) = .jstrVal(...)
+  if (is.character(obj))
+    return(obj)
   r<-NULL
   if (!inherits(obj,"jobjRef"))
     stop("can get value of Java objects only")
@@ -44,3 +63,4 @@
   r
 }
 
+  
