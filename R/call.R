1442e5e0692aae938943101eef66fc8f0b2df3a2
Simon Urbanek <simon.urbanek@r-project.org>
2003-09-18 18:20:41 +0000

    Fix bug in .jfree; add support for static methods; add evalString=TRUE option to .jcall
    git-svn-id: svn://svn.rforge.net/rJava/trunk@24 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- R/call.R
+++ R/call.R
@@ -12,40 +12,45 @@
   o
 }
 
-.jcall <- function(obj, returnSig="V", method, ..., evalArray=FALSE) {
+.jcall <- function(obj, returnSig="V", method, ..., evalArray=FALSE, evalString=TRUE) {
   r<-NULL
-  if (inherits(obj,"jobjRef")) {
-    if (returnSig=="S")
-      returnSig<-"Ljava/lang/String;"
-    if (returnSig=="[S")
-      returnSig<-"[Ljava/lang/String;"
+  if (returnSig=="S")
+    returnSig<-"Ljava/lang/String;"
+  if (returnSig=="[S")
+    returnSig<-"[Ljava/lang/String;"
+  if (inherits(obj,"jobjRef"))
     r<-.External("RcallMethod",obj$jobj,returnSig, method, ..., PACKAGE="rJava")
-    if (substr(returnSig,1,1)=="[") {
-      if (evalArray) {
-        if (returnSig=="[I")
-          return(.External("RgetIntArrayCont", r))
-        else if (returnSig=="[D")
-          return(.External("RgetDoubleArrayCont", r))
-        else if (substr(returnSig,1,2)=="[L") # this one is wrong - we'll get all the jobjects, but we'd have to convert them to R objects first
-          return(.External("RgetObjectArrayCont", r))
-      }
-      r<-list(jobj=r, jclass=NULL, jsig=returnSig)
-      class(r)<-c("jarrayRef","jobjRef")
-    } else if (substr(returnSig,1,1)=="L") {
-      r<-list(jobj=r, jclass=substr(returnSig,2,nchar(returnSig)-1))
-      class(r)<-"jobjRef"
+  else
+    r<-.External("RcallStaticMethod",as.character(obj), returnSig, method, ..., PACKAGE="rJava")
+  if (substr(returnSig,1,1)=="[") {
+    if (evalArray) {
+      if (returnSig=="[I")
+        return(.External("RgetIntArrayCont", r))
+      else if (returnSig=="[D")
+        return(.External("RgetDoubleArrayCont", r))
+      else if (substr(returnSig,1,2)=="[L") # this one is wrong - we'll get all the jobjects, but we'd have to convert them to R objects first
+        return(.External("RgetObjectArrayCont", r))
     }
-    
-    .C("checkExceptions")
-  } else { # call for static methods
+    r<-list(jobj=r, jclass=NULL, jsig=returnSig)
+    class(r)<-c("jarrayRef","jobjRef")
+  } else if (substr(returnSig,1,1)=="L") {
+    if (returnSig=="Ljava/lang/String;" && evalString) {
+      s<-.External("RgetStringValue",r)
+      .External("RfreeObject",r)
+      return(s)
+    }
+    r<-list(jobj=r, jclass=substr(returnSig,2,nchar(returnSig)-1))
+    class(r)<-"jobjRef"
   }
+    
+  .C("checkExceptions")
   r
 }
 
 .jfree <- function(obj) {
   if (!inherits(obj,"jobjRef"))
     stop("obj is not a Java object")
-  .External("RfreeObject",obj$obj)
+  .External("RfreeObject",obj$jobj)
   invisible()
 }
 
@@ -56,7 +61,7 @@
   r<-NULL
   if (!inherits(obj,"jobjRef"))
     stop("can get value of Java objects only")
-  if (obj$jclass=="lang/java/String")
+  if (!is.null(obj$jclass) && obj$jclass=="lang/java/String")
     r<-.External("RgetStringValue",obj$jobj)
   else
     r<-.External("RtoString",obj$jobj)
