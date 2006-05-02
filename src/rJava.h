a3f0599ae531690065abbb5aa4f4f0cd26356041
Simon Urbanek <simon.urbanek@r-project.org>
2006-05-02 02:51:22 +0000

    rJava 0.3-10 (fixed error in .jmergeClassPath and acceps PKG_LIBS)
    git-svn-id: svn://svn.rforge.net/rJava/trunk@138 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x000309 /* rJava v0.3-9 */
+#define RJAVA_VER 0x00030a /* rJava v0.3-10 */
 
 /* important changes between versions:
    0.3  - uses EXTPTR in jobj slot, adds finalizers
