136a89ec03a12ac40e4ee172b7c684cf8d5daf21
Simon Urbanek <simon.urbanek@r-project.org>
2006-05-03 15:47:26 +0000

    Include JRI in rJava and add support for --enable-debug
    git-svn-id: svn://svn.rforge.net/rJava/trunk@140 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x00030a /* rJava v0.3-10 */
+#define RJAVA_VER 0x00030b /* rJava v0.3-11 */
 
 /* important changes between versions:
    0.3  - uses EXTPTR in jobj slot, adds finalizers
