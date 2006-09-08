e210677edb37ccb808f5e3bb29760681aa600268
Simon Urbanek <simon.urbanek@r-project.org>
2006-09-08 16:56:49 +0000

    Update NEWS, version to 0.4-8 and cleanup
    git-svn-id: svn://svn.rforge.net/rJava/trunk@176 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x000407 /* rJava v0.4-7 */
+#define RJAVA_VER 0x000408 /* rJava v0.4-8 */
 
 /* important changes between versions:
    0.4  - includes JRI
