8d83535244f6f3688ee79956b3ce8d7856b786ee
Markus Helbig <info@markushelbig.de>
2006-09-12 08:22:58 +0000

    fixed: JRI version numbers
    git-svn-id: svn://svn.rforge.net/rJava/trunk@178 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x000408 /* rJava v0.4-8 */
+#define RJAVA_VER 0x000408 /* rJava v0.4-9 */
 
 /* important changes between versions:
    0.4  - includes JRI
