ff4086eba92b72da17d4fef0ab569d4167cd4992
Simon Urbanek <simon.urbanek@r-project.org>
2006-09-14 17:01:19 +0000

    rJava 0.4-10
    git-svn-id: svn://svn.rforge.net/rJava/trunk@182 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x000408 /* rJava v0.4-9 */
+#define RJAVA_VER 0x00040a /* rJava v0.4-10 */
 
 /* important changes between versions:
    0.4  - includes JRI
