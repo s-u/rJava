3c476ecf92d779a3793bfee802266b3c6ef497db
Simon Urbanek <simon.urbanek@r-project.org>
2007-01-08 15:40:14 +0000

    Move IPC client code to JRI, create callback header file
    git-svn-id: svn://svn.rforge.net/rJava/trunk@207 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/callback.c
+++ src/callback.c
@@ -1,18 +1,11 @@
 #include "rJava.h"
 #include <R_ext/eventloop.h>
 #include <unistd.h>
-
-#define RJavaActivity 16
+#include "callback.h"
 
 int RJava_has_control = 0;
 
 static int ipcin, ipcout, resin, resout;
-
-/* all IPC messages are long-alligned */
-#define IPCC_LOCK_REQUEST 1
-#define IPCC_LOCK_GRANTED 2 /* reponse on IPCC_LOCK_REQUEST */
-#define IPCC_CLEAR_LOCK   3
-#define IPCC_CALL_REQUEST 4 /* pars: <fn-ptr> <data-ptr> */
 
 typedef void(callbackfn)(void*);
 
@@ -27,6 +20,10 @@ static void RJava_ProcessEvents(void *data) {
   }
   if (buf[0] == IPCC_CLEAR_LOCK) {
     RJava_has_control = 0;
+  }
+  if (buf[0] == IPCC_CONTROL_ADDR) {
+    buf[1] = (long) (void*) &RJava_has_control;
+    write(resout, buf, sizeof(long)*2);
   }
   if (buf[0] == IPCC_CALL_REQUEST) {
     read(ipcin, buf+1, sizeof(long)*2);
@@ -46,31 +43,5 @@ int RJava_init_loop() {
   resin = pfd[0];
   resout = pfd[1];
   addInputHandler(R_InputHandlers, ipcin, RJava_ProcessEvents, RJavaActivity);
-}
-
-int RJava_request_lock() {
-  long buf[4];
-  int n;
-  if (RJava_has_control) return 1;
-  
-  buf[0] = IPCC_LOCK_REQUEST;
-  write(ipcout, buf, sizeof(long));
-  n = read(resin, buf, sizeof(long));
-  return (buf[0] == IPCC_LOCK_GRANTED);
-}
-
-int RJava_clear_lock() {
-  long buf[4];
-  buf[0] = IPCC_CLEAR_LOCK;
-  write(ipcout, buf, sizeof(long));
-  return 1;
-}
-
-void RJava_request_callback(callbackfn *fn, void *data) {
-  long buf[4];
-  buf[0] = IPCC_CALL_REQUEST;
-  buf[1] = (long) fn;
-  buf[2] = (long) data;
-  write(ipcout, buf, sizeof(long)*3);
 }
 
