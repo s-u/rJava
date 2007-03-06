b9e25b8784b4ed51655e38af531296745f65f6db
Simon Urbanek <simon.urbanek@r-project.org>
2007-03-06 22:47:13 +0000

    add .jengine to fetch/start JRI and (unexported) .setupJRI which does the real job
    git-svn-id: svn://svn.rforge.net/rJava/trunk@258 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- R/jri.R
+++ R/jri.R
@@ -7,3 +7,45 @@
   new("jobjRef",jobj=.Call("PushToREXP","org/rosuda/JRI/REXP",engine@jobj,engine@jclass,x,PACKAGE="rJava"),jclass="org/rosuda/JRI/REXP")
 }
 
+.setupJRI <- function(new=TRUE) {
+  ec <- .jfindClass("org.rosuda.JRI.Rengine", silent=TRUE)
+  if (is.jnull(ec)) {
+    .jcheck(TRUE)
+    .jaddClassPath(system.file("jri","JRI.jar",package="rJava"))
+    ec <- .jfindClass("org.rosuda.JRI.Rengine", silent=TRUE)
+    .jcheck(TRUE)
+    if (is.jnull(ec))
+      stop("Cannot find JRI classes")
+  }
+  me <- .jcall("org/rosuda/JRI/Rengine","Lorg/rosuda/JRI/Rengine;","getMainEngine", check=FALSE)
+  .jcheck(TRUE)
+  if (!is.jnull(me)) {
+    if (!new) return(TRUE)
+    warning("JRI engine is already running.")
+    return(FALSE)
+  }
+  e <- .jnew("org/rosuda/JRI/Rengine")
+  !is.jnull(e)
+}
+
+.jengine <- function(start=FALSE, silent=FALSE) {
+  me <- NULL
+  ec <- .jfindClass("org.rosuda.JRI.Rengine", silent=TRUE)
+  .jcheck(TRUE)
+  if (!is.jnull(ec)) {
+    me <- .jcall("org/rosuda/JRI/Rengine","Lorg/rosuda/JRI/Rengine;","getMainEngine", check=FALSE)
+    .jcheck(TRUE)
+  }
+  if (is.jnull(me)) {
+    if (!start) {
+      if (silent) return(NULL)
+      stop("JRI engine is not running.")
+    }
+    .setupJRI(FALSE)
+    me <- .jcall("org/rosuda/JRI/Rengine","Lorg/rosuda/JRI/Rengine;","getMainEngine", check=FALSE)
+    .jcheck(TRUE)
+  }
+  if (is.jnull(me) && !silent)
+    stop("JRI engine is not running.")
+  me
+}
