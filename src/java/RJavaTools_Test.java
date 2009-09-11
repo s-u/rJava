076a89da838b94520c4fddf413a1a2b653363154
Romain Francios <romain@r-enthusiasts.com>
2009-09-11 08:52:56 +0000

    testing RJavaTools
    git-svn-id: svn://svn.rforge.net/rJava/trunk@380 634b503b-a89c-4e0f-88f7-82c2bf7838a2

---
+++ src/java/RJavaTools_Test.java
@@ -0,0 +1,114 @@
+// :tabSize=2:indentSize=2:noTabs=false:folding=explicit:collapseFolds=1:
+import javax.swing.JFrame ;
+import java.awt.Point ;
+import java.lang.reflect.Constructor ;
+import javax.swing.JButton ;
+import javax.swing.ImageIcon ;
+
+public class RJavaTools_Test {
+
+	// {{{ main 
+	public static void main( String[] args){
+		
+		System.out.println( "Testing RJavaTools.getConstructor" ) ;
+		try{
+			constructors() ;
+		} catch( TestException e ){
+			System.err.println( "FAILED" ) ; 
+		}
+		System.out.println( "PASSED" ) ;
+		
+		System.out.println( "Testing RJavaTools.getMethod" ) ;
+		try{
+			methods() ;
+		} catch( TestException e ){
+			System.err.println( "FAILED" ) ; 
+		}
+		System.out.println( "PASSED" ) ;
+		
+	}
+	// }}}
+
+  // {{{ @Test constructors 
+	public static void constructors() throws TestException {
+		/* constructors */ 
+		Constructor cons ;
+		boolean error ; 
+		
+		// {{{ getConstructor( String, null )
+		System.out.print( "    * getConstructor( String, null )" ) ;
+		try{
+			cons = RJavaTools.getConstructor( String.class, (Class[])null ) ;
+		} catch( Exception e ){
+			throw new TestException( "getConstructor( String, null )" ) ; 
+		}
+		System.out.println( " : ok " ) ;
+		// }}}
+		
+		// {{{ getConstructor( Integer, { String.class } ) 
+		System.out.print( "    * getConstructor( Integer, { String.class } )" ) ;
+		try{
+			cons = RJavaTools.getConstructor( Integer.class, new Class[]{ String.class } ) ;
+		} catch( Exception e){
+			throw new TestException( "getConstructor( Integer, { String.class } )" ) ; 
+		}
+		System.out.println( " : ok " ) ;
+		// }}}
+		
+		// {{{ getConstructor( JButton, { String.class, ImageIcon.class } )
+		System.out.print( "    * getConstructor( JButton, { String.class, ImageIcon.class } )" ) ;
+		try{
+			cons = RJavaTools.getConstructor( JButton.class, new Class[]{ String.class, ImageIcon.class } ) ;
+		} catch( Exception e){
+			throw new TestException( "getConstructor( JButton, { String.class, ImageIcon.class } )" ) ; 
+		}
+		System.out.println( " : ok " ) ;
+		// }}}
+		
+		// {{{ getConstructor( Integer, null ) -> exception 
+		error = false ; 
+		System.out.print( "    * getConstructor( Integer, null )" ) ;
+		try{
+			cons = RJavaTools.getConstructor( Integer.class, (Class[])null ) ;
+		} catch( Exception e){
+			 error = true ; 
+		}
+		if( !error ){
+			throw new TestException( "getConstructor( Integer, null ) did not generate error" ) ;
+		}
+		System.out.println( " -> exception : ok " ) ;
+		// }}}
+		
+		// {{{ getConstructor( JButton, { String.class, JButton.class } ) -> exception
+		error = false ; 
+		System.out.print( "    * getConstructor( JButton, { String.class, JButton.class } )" ) ;
+		try{
+			cons = RJavaTools.getConstructor( JButton.class, new Class[]{ String.class, JButton.class } ) ;
+		} catch( Exception e){
+			 error = true ; 
+		}
+		if( !error ){
+			throw new TestException( "getConstructor( JButton, { String.class, JButton.class } ) did not generate error" ) ;
+		}
+		System.out.println( " -> exception : ok " ) ;
+		// }}}
+		
+	}
+	// }}}
+	
+	// {{{ @Test methods
+	private static void methods() throws TestException{
+		
+	}
+	// }}}
+
+	// {{{ TestException class
+	private static class TestException extends Exception{
+		public TestException( String message ){
+			super( message ) ;
+		}
+	}
+	// }}}
+	
+
+}
