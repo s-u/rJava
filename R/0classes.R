87ff59360a945155f7d93d0117003c5089cf30e0
Romain Francios <romain@r-enthusiasts.com>
2009-09-25 07:01:58 +0000

    added jrectRef class (extends jarrayRef) to deal with the rectangular java arrays - no code yet
    git-svn-id: svn://svn.rforge.net/rJava/trunk@432 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- R/0classes.R
+++ R/0classes.R
@@ -1,4 +1,4 @@
-## S4 classes (jobjRef is re-defined in .Frist.lib to contain valid jobj)                                                                                                  
+## S4 classes (jobjRef is re-defined in .First.lib to contain valid jobj)                                                                                                  
 setClass("jobjRef", representation(jobj="externalptr", jclass="character"), prototype=list(jobj=NULL, jclass="java/lang/Object"))
 setClass("jarrayRef", representation("jobjRef", jsig="character"))
 setClass("jfloat", representation("numeric"))
@@ -6,3 +6,7 @@ setClass("jlong", representation("numeric"))
 setClass("jbyte", representation("integer"))
 setClass("jshort", representation("integer"))
 setClass("jchar", representation("integer"))
+
+#' rectangular java arrays double[][] d = new double[m][n]
+setClass("jrectRef", representation("jarrayRef", dimension="integer") ) 
+
