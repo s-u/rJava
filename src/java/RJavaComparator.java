ebeeb59e01521f3114fc2ca832ef9c439e94e7d9
Romain Francios <romain@r-enthusiasts.com>
2009-10-11 10:09:21 +0000

    allow comparison of Numbers of different types
    git-svn-id: svn://svn.rforge.net/rJava/trunk@475 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/java/RJavaComparator.java
+++ src/java/RJavaComparator.java
@@ -1,10 +1,37 @@
 import java.lang.Comparable ; 
 
+/**
+ * Utility class to compare two objects in the sense
+ * of the java.lang.Comparable interface
+ *
+ */
 public class RJavaComparator {
-	
+	           
+	/**
+	 * compares a and b in the sense of the java.lang.Comparable if possible
+	 *
+	 * <p>instances of the Number interface are treated specially, in order to 
+	 * allow comparing Numbers of different classes, for example it is allowed 
+	 * to compare a Double with an Integer. if the Numbers have the same class,
+	 * they are compared normally, otherwise they are first converted to Doubles
+	 * and then compared</p>
+	 * 
+	 * @param a an object
+	 * @param b another object 
+	 * 
+	 * @return the result of <code>a.compareTo(b)</code> if this makes sense
+	 * @throws NotComparableException if the two objects are not comparable
+	 */
 	public static int compare( Object a, Object b ) throws NotComparableException{
 		int res ; 
 		if( a.equals( b ) ) return 0 ;
+		
+		// treat Number s separately
+		if( a instanceof Number && b instanceof Number && !( a.getClass() == b.getClass() ) ){
+			Double _a = new Double( ((Number)a).doubleValue() );
+			Double _b = new Double( ((Number)b).doubleValue() );
+			return _a.compareTo( _b ); 
+		}
 		
 		if( ! ( a instanceof Comparable ) ) throw new NotComparableException( a ); 
 		if( ! ( b instanceof Comparable ) ) throw new NotComparableException( b ); 
@@ -21,6 +48,12 @@ public class RJavaComparator {
 		return res ;
 	}
 	
+	/**
+	 * Exception generated when two objects cannot be compared
+	 * 
+	 * Such cases happen when an object does not implement the Comparable 
+	 * interface or when the comparison produces a ClassCastException
+	 */
 	public static class NotComparableException extends Exception{
 		public NotComparableException(Object a, Object b){
 			super( "objects of class " + a.getClass().getName() + 
@@ -28,9 +61,6 @@ public class RJavaComparator {
 		}
 		public NotComparableException( Object o){
 			super( "class " + o.getClass().getName() + " does not implement java.util.Comparable" ) ; 
-		}
-		public NotComparableException( Class cl){
-			super( "primitive" + cl.getName() + " not comparable to object" ); 
 		}
 	}
 }
