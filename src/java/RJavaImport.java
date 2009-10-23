f54a04d8892df4f4995c2ca537c24f599989afd1
Romain Francios <romain@r-enthusiasts.com>
2009-10-23 21:04:24 +0000

    more javadoc
    git-svn-id: svn://svn.rforge.net/rJava/trunk@500 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- src/java/RJavaImport.java
+++ src/java/RJavaImport.java
@@ -11,25 +11,55 @@ import java.util.Iterator ;
 
 import java.io.Serializable; 
 
+/**
+ * Utilities to manage java packages and how they are "imported" to R
+ * databases. This is the back door of the <code>javaImport</code>
+ * system in the R side
+ *
+ * @author Romain Francois <francoisromain@free.fr>
+ */
 public class RJavaImport implements Serializable {
 
+	/**
+	 * Debug flag. Prints some messages if it is set to TRUE
+	 */
 	public static boolean DEBUG = false ; 
 	
+	/** 
+	 * list of imported packages
+	 */
 	/* TODO: vector is not good enough, we need to control the order
 	         in which the packages appear */
 	private Vector/*<Package>*/ importedPackages ;
 	
-	/* maps a simple name to a fully qualified name
-		String -> java.lang.String 
-	*/
-	/* should we cache the Class instead ? */
+	/**
+	 * maps a simple name to a fully qualified name
+	 */
+	/* String -> java.lang.String */
+  /* should we cache the Class instead ? */
 	private Map/*<String,String>*/ cache ;
 	
+	/**
+	 * Constructor. Initializes the imported package vector and the cache
+	 */ 
 	public RJavaImport( ){
 		importedPackages = new Vector/*<Package>*/(); 
 		cache = new HashMap/*<String,String>*/() ;
 	}
 	
+	/**
+	 * Look for the class in the set of packages
+	 *
+	 * @param clazz the simple class name
+	 * 
+	 * @return an instance of Class representing the actual class
+	 */
+	public Class lookup( String clazz){
+		Class res = lookup_(clazz) ;
+		if( DEBUG ) System.out.println( "  [J] lookup( '" + clazz + "' ) = " + (res == null ? " " : ("'" + res.getName() + "'" ) ) ) ;
+		return res ;
+	}
+
 	private Class lookup_( String clazz ){
 		Class res = null ;
 		
@@ -68,25 +98,26 @@ public class RJavaImport implements Serializable {
 		return null ;
 	}
 	
-	public Class lookup( String clazz){
-		Class res = lookup_(clazz) ;
-		if( DEBUG ) System.out.println( "  [J] lookup( '" + clazz + "' ) = " + (res == null ? " " : ("'" + res.getName() + "'" ) ) ) ;
-		return res ;
-	}
-
-	/* TODO: we don't actually have to instantiate the Class */
-	public boolean exists_( String clazz ){
-		if( cache.containsKey( clazz ) ) return true ;
-		return ( lookup( clazz ) != null );
-	}
-	
+	/**
+	 * @return true if the class is known
+	 */
 	public boolean exists( String clazz){
 		boolean res = exists_(clazz) ;
 		if( DEBUG ) System.out.println( "  [J] exists( '" + clazz + "' ) = " + res ) ;
 		return res ;
 	}
 
+	public boolean exists_( String clazz ){
+		if( cache.containsKey( clazz ) ) return true ;
+		
+		return ( lookup_( clazz ) != null );
+	}
 	
+	/**
+	 * Adds a package to the list of "imported" packages
+	 *
+	 * @param packageName package path name
+	 */
   public void importPackage( String packageName ){
   	Package p  = Package.getPackage( packageName ) ;
   	if( p != null ){
@@ -94,6 +125,11 @@ public class RJavaImport implements Serializable {
   	}
   }
 	
+  /**
+   * Adds a set of packages
+   *
+   * @param packages package path names
+   */
   public void importPackage( String[] packages ){
   	for( int i=0; i<packages.length; i++){
   		importPackage( packages[i] ) ;
@@ -101,7 +137,7 @@ public class RJavaImport implements Serializable {
   }
   
   /**
-   * @return the full names of the classes currently known
+   * @return the simple names of the classes currently known
    * by this importer
    */
   public String[] getKnownClasses(){
