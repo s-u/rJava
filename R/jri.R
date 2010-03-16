d25752254e3868b8dd9863fbd304ed70e4273be7
Simon Urbanek <simon.urbanek@r-project.org>
2010-03-16 17:56:02 +0000

    add toJava and include REngine+JRIEngine in the distribution
    git-svn-id: svn://svn.rforge.net/rJava/trunk@537 634b503b-a89c-4e0f-88f7-82c2bf7838a2

--- R/jri.R
+++ R/jri.R
@@ -1,10 +1,45 @@
 ## bindings into JRI
 
 ## warning: JRI REXP class has currently no finalizers! (RReleaseREXP must be used manually for now)
+## warning: this produces JRI-API pbjects - that should go away! use toJava below
 .r2j <- function(x, engine = NULL, convert = TRUE) {
   if (is.null(engine)) engine <- .jcall("org/rosuda/JRI/Rengine","Lorg/rosuda/JRI/Rengine;","getMainEngine")
   if (!is(engine, "jobjRef")) stop("invalid or non-existent engine")
   new("jobjRef",jobj=.Call("PushToREXP","org/rosuda/JRI/REXP",engine@jobj,engine@jclass,x,convert,PACKAGE="rJava"),jclass="org/rosuda/JRI/REXP")
+}
+
+toJava <- function(x, engine = NULL) {
+  ## this is really the wrong place for all this REngine checking stuff, but so far .jengine uses JRI API only and legacy code may rely on that
+  ## so this is the only place that assumes REngine API and thus will load it ...
+  ec <- .jfindClass("org.rosuda.JRI.Rengine", silent=TRUE)
+  if (is.jnull(ec)) {
+    .jcheck(TRUE)
+    stop("JRI is not loaded. Please start JRI first - see ?.jengine")
+  }
+  ec <- .jfindClass("org.rosuda.REngine.REngine", silent=TRUE)
+  if (is.jnull(ec)) {
+    .jcheck(TRUE)
+    fn <- system.file("jri","REngine.jar",package="rJava")
+    if (nzchar(fn)) .jaddClassPath(fn)
+    fn <- system.file("jri","JRIEngine.jar",package="rJava")
+    if (nzchar(fn)) .jaddClassPath(fn)
+    ec <- .jfindClass("org.rosuda.REngine.REngine", silent=TRUE)
+    if (is.jnull(ec)) {
+      .jcheck(TRUE)
+      stop("Cannot find REngine API classes. Please make sure you have installed and loaded the REngine API")
+    }
+  }
+  if (is.null(engine)) engine <- .jcall("org/rosuda/REngine/REngine","Lorg/rosuda/REngine/REngine;","getLastEngine")
+  if (is.jnull(engine)) { # no last engine, but there may be JRI engine already running ...
+    me <- .jcall("org/rosuda/JRI/Rengine","Lorg/rosuda/JRI/Rengine;","getMainEngine", check=FALSE)
+    .jcheck(TRUE)
+    if (is.jnull(me)) stop("JRI is not running. Please start JRI first - see ?.jengine")
+    engine <- .jnew("org/rosuda/REngine/JRI/JRIEngine", me)
+    .jcheck(TRUE)
+  }
+  .jcheck(TRUE)
+  if (!is(engine, "jobjRef")) stop("invalid or non-existent engine")
+  new("jobjRef",jobj=.Call("PushToREXP","org/rosuda/REngine/REXPReference",engine@jobj,"org/rosuda/REngine/REngine",x,NULL,PACKAGE="rJava"),jclass="org/rosuda/REngine/REXPReference")
 }
 
 .setupJRI <- function(new=TRUE) {
