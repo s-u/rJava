79226961f74a71a0798efc012c0efb704f3dec9d
Simon Urbanek <simon.urbanek@r-project.org>
2014-01-02 13:55:28 -0500

    bump the version

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x000906 /* rJava v0.9-6 */
+#define RJAVA_VER 0x000907 /* rJava v0.9-7 */
 
 /* important changes between versions:
    3.0  - adds compiler
