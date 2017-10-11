e7f5c41fbd0b94981845430050a75d4b747f33e0
Simon Urbanek <simon.urbanek@r-project.org>
2017-10-11 15:45:35 -0400

    Win: look in JDK/JRE registry as used by Java 1.9 (#120)

--- R/windows/FirstLib.R
+++ R/windows/FirstLib.R
@@ -5,8 +5,12 @@ function(libname, pkgname) {
     if(!nchar(javahome)) { ## JAVA_HOME was not set explicitly
         find.java <- function() {
             for (root in c("HLM", "HCU"))
-                for(key in c("Software\\JavaSoft\\Java Runtime Environment",
-                             "Software\\JavaSoft\\Java Development Kit")) {
+                for(key in c(
+                               "Software\\JavaSoft\\JRE",
+                               "Software\\JavaSoft\\JDK",
+                               "Software\\JavaSoft\\Java Runtime Environment",
+                               "Software\\JavaSoft\\Java Development Kit"
+                             )) {
                   hive <- try(utils::readRegistry(key, root, 2), silent=TRUE)
                   if (!inherits(hive, "try-error")) return(hive)
                 }
