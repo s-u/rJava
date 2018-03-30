1c95cff8cdd71cbff6a3f01dc0935a3d892461f7
Simon Urbanek <simon.urbanek@R-project.org>
2018-03-30 14:28:54 -0400

    bump to rJava 0.9-10

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x000909 /* rJava v0.9-9 */
+#define RJAVA_VER 0x00090a /* rJava v0.9-10 */
 
 /* important changes between versions:
    3.0  - adds compiler
