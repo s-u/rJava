0e55a0605942970d15f5fcdade661a6070c2b6a5
Simon Urbanek <simon.urbanek@r-project.org>
2019-03-25 17:37:24 -0400

    bump version to 0.9-11

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x00090a /* rJava v0.9-10 */
+#define RJAVA_VER 0x00090b /* rJava v0.9-11 */
 
 /* important changes between versions:
    3.0  - adds compiler
