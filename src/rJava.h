571cd482e4503543504c14283b545382a48f0382
Simon Urbanek <simon.urbanek@R-project.org>
2021-04-29 02:13:24 +0000

    bump version to 1.0-4

--- src/rJava.h
+++ src/rJava.h
@@ -1,7 +1,7 @@
 #ifndef __RJAVA_H__
 #define __RJAVA_H__
 
-#define RJAVA_VER 0x010003 /* rJava v1.0-3 */
+#define RJAVA_VER 0x010004 /* rJava v1.0-4 */
 
 /* important changes between versions:
    3.0  - adds compiler
